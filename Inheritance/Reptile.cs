0c59c7af2e48bad3111005af26aeff942aa64d30
Anthony Allgood <62567954+Allyogi84@users.noreply.github.com>
2020-09-03 14:19:28 -0400

    my new code files

---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Inheritance
+{
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+
+            IsAlive = true;
+            LegCount = 4;
+            LandSeaAir = "Land";
+            Age = 0;
+
+
+
+        }
+
+        public bool IsColdBlooded { get; set; }
+        public bool Isscaley { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+        public bool IsScaly { get; internal set; }
+    }
+}
